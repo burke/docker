c041603a1bba4746a90fc8c63279028d3c57603f
Burke Libbey <burke@libbey.me>
2015-04-14 15:28:54 -0400

    archive: Optimize ChangesDirs on Linux
    If we tear through a few layers of abstraction, we can get at the inodes
    contained in a directory without having to stat all the files. This
    allows us to eliminate identical files much earlier in the changelist
    generation process.
    
    Signed-off-by: Burke Libbey <burke@libbey.me>

---
+++ pkg/archive/walker.go
@@ -0,0 +1,315 @@
+package archive
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+	"syscall"
+	"unsafe"
+
+	"github.com/docker/docker/pkg/system"
+)
+
+type WalkFunc func(path string, oldInfo, newInfo os.FileInfo, err error) error
+
+type walker struct {
+	root1 string
+	root2 string
+	fn    WalkFunc
+}
+
+// collectFileInfoForChanges returns a complete representation of the trees
+// rooted at dir1 and dir2, with one important exception: any subtree or
+// leaf where the inode and device numbers are an exact match between dir1
+// and dir2 will be pruned from the results. This method is *only* to be used
+// to generating a list of changes between the two directories, as it does not
+// reflect the full contents.
+func collectFileInfoForChanges(dir1, dir2 string) (*FileInfo, *FileInfo, error) {
+	root1 := newRootFileInfo()
+	root2 := newRootFileInfo()
+
+	err := filepathWalk(dir1, dir2, func(path string, f1, f2 os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if path == "/" {
+			return nil
+		}
+
+		if f1 != nil {
+			parent1 := root1.LookUp(filepath.Dir(path))
+			if parent1 == nil {
+				return fmt.Errorf("collectFileInfoForChanges: Unexpectedly no parent for %s", path)
+			}
+			info1 := &FileInfo{
+				name:     filepath.Base(path),
+				children: make(map[string]*FileInfo),
+				parent:   parent1,
+			}
+			path1 := filepath.Join(dir1, path)
+			stat1, err := system.Lstat(path1)
+			if err != nil {
+				return err
+			}
+			info1.stat = stat1
+			info1.capability, _ = system.Lgetxattr(path1, "security.capability")
+			parent1.children[info1.name] = info1
+		}
+
+		if f2 != nil {
+			parent2 := root2.LookUp(filepath.Dir(path))
+			if parent2 == nil {
+				return fmt.Errorf("collectFileInfoForChanges: Unexpectedly no parent for %s", path)
+			}
+			info2 := &FileInfo{
+				name:     filepath.Base(path),
+				children: make(map[string]*FileInfo),
+				parent:   parent2,
+			}
+			path2 := filepath.Join(dir2, path)
+			stat2, err := system.Lstat(path2)
+			if err != nil {
+				return err
+			}
+			info2.stat = stat2
+			info2.capability, _ = system.Lgetxattr(path2, "security.capability")
+			parent2.children[info2.name] = info2
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	return root1, root2, nil
+}
+
+func filepathWalk(root1, root2 string, walkFn WalkFunc) error {
+	walker := &walker{
+		root1: root1,
+		root2: root2,
+		fn:    walkFn,
+	}
+
+	i1, err := os.Lstat(root1)
+	if err != nil {
+		return walkFn("/", nil, nil, err)
+	}
+	i2, err := os.Lstat(root2)
+	if err != nil {
+		return walkFn("/", nil, nil, err)
+	}
+
+	return walker.walk("/", i1, i2)
+}
+
+func (w *walker) walk(path string, i1, i2 os.FileInfo) error {
+	err := w.fn(path, i1, i2, nil)
+	if err != nil {
+		return err
+	}
+
+	// If these files (and therefore the subtrees rooted here) are the same
+	// inode, we need not walk them.
+	if i1 != nil && i2 != nil && os.SameFile(i1, i2) {
+		return nil
+	}
+
+	is1Dir := i1 != nil && i1.IsDir()
+	is2Dir := i2 != nil && i2.IsDir()
+
+	// If these files are both non-existent, or leaves (non-dirs), we are done.
+	if !is1Dir && !is2Dir {
+		return nil
+	}
+
+	var names1, names2 []nameIno
+	if is1Dir {
+		names1, err = readDirNames(filepath.Join(w.root1, path))
+		if err != nil {
+			return w.fn(path, i1, i2, err)
+		}
+	}
+
+	if is2Dir {
+		names2, err = readDirNames(filepath.Join(w.root2, path))
+		if err != nil {
+			return w.fn(path, i1, i2, err)
+		}
+	}
+
+	var names []string
+	ix1 := 0
+	ix2 := 0
+
+	for {
+		if ix1 >= len(names1) {
+			break
+		}
+		if ix2 >= len(names2) {
+			break
+		}
+
+		ni1 := names1[ix1]
+		ni2 := names2[ix2]
+
+		switch bytes.Compare([]byte(ni1.name), []byte(ni2.name)) {
+		case -1: // ni1 < ni2 -- advance ni1
+			// we will not encounter ni1 in names2
+			names = append(names, ni1.name)
+			ix1++
+		case 0: // ni1 == ni2
+			if ni1.ino != ni2.ino {
+				names = append(names, ni1.name)
+			}
+			ix1++
+			ix2++
+		case 1: // ni1 > ni2 -- advance ni2
+			// we will not encounter ni2 in names1
+			names = append(names, ni2.name)
+			ix2++
+		}
+	}
+	for ix1 < len(names1) {
+		names = append(names, names1[ix1].name)
+		ix1++
+	}
+	for ix2 < len(names2) {
+		names = append(names, names2[ix2].name)
+		ix2++
+	}
+
+	for _, name := range names {
+		fname := filepath.Join(path, name)
+		fp1 := filepath.Join(w.root1, fname)
+		fp2 := filepath.Join(w.root2, fname)
+		oldInfo, err1 := os.Lstat(fp1)
+		newInfo, err2 := os.Lstat(fp2)
+		if err1 != nil && !os.IsNotExist(err1) {
+			if err := w.fn(fname, oldInfo, newInfo, err1); err != nil {
+				return err
+			}
+		} else if err2 != nil && !os.IsNotExist(err2) {
+			if err := w.fn(fname, oldInfo, newInfo, err2); err != nil {
+				return err
+			}
+		} else {
+			err = w.walk(fname, oldInfo, newInfo)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func readDirNames(dirname string) ([]nameIno, error) {
+	f, err := os.Open(dirname)
+	if err != nil {
+		return nil, err
+	}
+	names, err := readdirnames(f)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	sl := nameInoSlice(names)
+	sort.Sort(sl)
+	return sl, nil
+}
+
+const (
+	blockSize = 4096
+)
+
+type dirInfo struct {
+	buf  []byte // buffer for directory I/O
+	nbuf int    // length of buf; return value from Getdirentries
+	bufp int    // location of next record in buf.
+}
+
+type nameIno struct {
+	name string
+	ino  uint64
+}
+
+type nameInoSlice []nameIno
+
+func (s nameInoSlice) Len() int           { return len(s) }
+func (s nameInoSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s nameInoSlice) Less(i, j int) bool { return s[i].name < s[j].name }
+
+func readdirnames(f *os.File) (names []nameIno, err error) {
+	size := 100
+	n := -1
+
+	d := new(dirInfo)
+	d.buf = make([]byte, 4096)
+
+	names = make([]nameIno, 0, size) // Empty with room to grow.
+	for n != 0 {
+		// Refill the buffer if necessary
+		if d.bufp >= d.nbuf {
+			d.bufp = 0
+			var errno error
+			d.nbuf, errno = fixCount(syscall.ReadDirent(int(f.Fd()), d.buf)) // getdents on linux
+			if errno != nil {
+				return names, os.NewSyscallError("readdirent", errno)
+			}
+			if d.nbuf <= 0 {
+				break // EOF
+			}
+		}
+
+		// Drain the buffer
+		var nb, nc int
+		nb, nc, names = ParseDirent(d.buf[d.bufp:d.nbuf], n, names)
+		d.bufp += nb
+		n -= nc
+	}
+	if n >= 0 && len(names) == 0 {
+		return names, io.EOF
+	}
+	return names, nil
+}
+
+func fixCount(n int, err error) (int, error) {
+	if n < 0 {
+		n = 0
+	}
+	return n, err
+}
+
+func ParseDirent(buf []byte, max int, names []nameIno) (consumed int, count int, newnames []nameIno) {
+	origlen := len(buf)
+	count = 0
+	for max != 0 && len(buf) > 0 {
+		dirent := (*syscall.Dirent)(unsafe.Pointer(&buf[0]))
+		buf = buf[dirent.Reclen:]
+		if dirent.Ino == 0 { // File absent in directory.
+			continue
+		}
+		bytes := (*[10000]byte)(unsafe.Pointer(&dirent.Name[0]))
+		var name = string(bytes[0:clen(bytes[:])])
+		if name == "." || name == ".." { // Useless names
+			continue
+		}
+		max--
+		count++
+		names = append(names, nameIno{name, dirent.Ino})
+	}
+	return origlen - len(buf), count, names
+}
+
+func clen(n []byte) int {
+	for i := 0; i < len(n); i++ {
+		if n[i] == 0 {
+			return i
+		}
+	}
+	return len(n)
+}
