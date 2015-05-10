16e3f0e4efee303d5a8dc0f3c0e378a5da4a2584
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-05-11 01:22:44 +0900

    Fix to get options from `--log-opt` command line option * port: fluentd port [24224] * host: fluentd host [localhost] * tag: tag of fluentd logs [docker.CONTAINER_ID_SHORT]        this value overrides any specifications for tag-prefix/tag-type * tag-prefix: prefix part of tag [docker] * tag-type: second part of tag, options are id/fullid/name [id]
    Signed-off-by: TAGOMORI Satoshi <tagomoris@gmail.com>

--- daemon/logger/fluentd/fluentd.go
+++ daemon/logger/fluentd/fluentd.go
@@ -1,22 +1,52 @@
 package fluentd
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/logger"
 	"github.com/fluent/fluent-logger-golang/fluent"
+	"strconv"
 )
 
 type Fluentd struct {
+	tag           string
 	containerid   string
 	containername string
 	writer        *fluent.Fluent
 }
 
-func New(id string, name string) (logger.Logger, error) {
-	log, err := fluent.New(fluent.Config{FluentPort: 24224, FluentHost: "localhost"})
+func New(config map[string]string, id string, name string) (logger.Logger, error) {
+	port := 24224
+	host := "localhost"
+	if config["port"] != "" {
+		port, _ = strconv.Atoi(config["port"])
+	}
+	if config["host"] != "" {
+		host = config["host"]
+	}
+	tag := config["tag"]
+	if tag == "" {
+		tagPrefix := "docker."
+		if config["tag-prefix"] != "" {
+			tagPrefix = config["tag-prefix"] + "."
+		}
+		if config["tag-type"] == "name" {
+			tag = tagPrefix + name
+		} else if config["tag-type"] == "fullid" {
+			tag = tagPrefix + id
+		} else { // id
+			tag = tagPrefix + id[:12]
+		}
+	}
+	log, err := fluent.New(fluent.Config{FluentPort: port, FluentHost: host})
+	logrus.Debugf("logging driver fluentd configured for container:%s.", id)
+	logrus.Debugf("logging driver fluentd port:%d.", port)
+	logrus.Debugf("logging driver fluentd host:%s.", host)
+	logrus.Debugf("logging driver fluentd tag:%s.", tag)
 	if err != nil {
 		return nil, err
 	}
 	return &Fluentd{
+		tag:           tag,
 		containerid:   id,
 		containername: name,
 		writer:        log,
@@ -24,14 +54,13 @@ func New(id string, name string) (logger.Logger, error) {
 }
 
 func (f *Fluentd) Log(msg *logger.Message) error {
-	tag := "docker." + f.containerid[:12] + "." + msg.Source
 	var data = map[string]string{
 		"container_id":   f.containerid,
 		"container_name": f.containername,
 		"source":         msg.Source,
 		"log":            string(msg.Line),
 	}
-	f.writer.PostWithTime(tag, msg.Timestamp, data)
+	f.writer.PostWithTime(f.tag, msg.Timestamp, data)
 	// fluent-logger-golang buffers logs from failures and disconnections,
 	// and these are transferred again automatically.
 	return nil
