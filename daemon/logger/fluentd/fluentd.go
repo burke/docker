28314475b803f40398653153799a05c76f52ff13
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-05-25 20:53:28 +0900

    Fix how to configure driver with review comments * move parser code for `--log-opt` values into a separated function * use `fluentd-` prefix for all options * use `fluentd-address` only to specify host/port of Fluentd server * use `fluentd-tag` with text/template syntax to format Fluentd tags
    Signed-off-by: TAGOMORI Satoshi <tagomoris@gmail.com>

--- daemon/logger/fluentd/fluentd.go
+++ daemon/logger/fluentd/fluentd.go
@@ -1,8 +1,12 @@
 package fluentd
 
 import (
+	"bytes"
 	"io"
+	"net"
 	"strconv"
+	"strings"
+	"text/template"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/logger"
@@ -14,6 +18,12 @@ type Fluentd struct {
 	containerID   string
 	containerName string
 	writer        *fluent.Fluent
+}
+
+type Receiver struct {
+	ID     string
+	FullID string
+	Name   string
 }
 
 const name = "fluentd"
@@ -28,40 +38,63 @@ func init() {
 	}
 }
 
-func New(ctx logger.Context) (logger.Logger, error) {
-	config := ctx.Config
-	port := defaultPort
+func parseConfig(ctx logger.Context) (string, int, string, error) {
 	host := defaultHostName
-	if config["port"] != "" {
-		portp, errp := strconv.Atoi(config["port"])
-		if errp != nil {
-			return nil, errp
-		}
-		port = portp
-	}
-	if config["host"] != "" {
-		host = config["host"]
-	}
-	tag := config["tag"]
-	if tag == "" {
-		tagPrefix := defaultTagPrefix
-		if config["tag-prefix"] != "" {
-			tagPrefix = config["tag-prefix"]
-		}
-		switch config["tag-type"] {
-		case "name":
-			tag = tagPrefix + "." + ctx.ContainerName
-		case "fullid":
-			tag = tagPrefix + "." + ctx.ContainerID
-		default:
-			tag = tagPrefix + "." + ctx.ContainerID[:12]
+	port := defaultPort
+	tag := "docker." + ctx.ContainerID[:12]
+
+	config := ctx.Config
+
+	if config["fluentd-address"] != "" {
+		address := config["fluentd-address"]
+		if h, p, err := net.SplitHostPort(address); err != nil {
+			if strings.Contains(err.Error(), "missing port in address") {
+				host = h
+			} else {
+				return "", 0, "", err
+			}
+		} else {
+			portnum, e := strconv.Atoi(p)
+			if e != nil {
+				return "", 0, "", e
+			}
+			host = h
+			port = portnum
 		}
 	}
-	log, err := fluent.New(fluent.Config{FluentPort: port, FluentHost: host})
+
+	if config["fluentd-tag"] != "" {
+		receiver := &Receiver{
+			ID:     ctx.ContainerID[:12],
+			FullID: ctx.ContainerID,
+			Name:   ctx.ContainerName,
+		}
+		tmpl, err := template.New("tag").Parse(config["fluentd-tag"])
+		if err != nil {
+			return "", 0, "", err
+		}
+		buf := new(bytes.Buffer)
+		err = tmpl.Execute(buf, receiver)
+		if err != nil {
+			return "", 0, "", err
+		}
+		tag = buf.String()
+	}
+
+	return host, port, tag, nil
+}
+
+func New(ctx logger.Context) (logger.Logger, error) {
+	host, port, tag, err := parseConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
 	logrus.Debugf("logging driver fluentd configured for container:%s.", ctx.ContainerID)
 	logrus.Debugf("logging driver fluentd port:%d.", port)
 	logrus.Debugf("logging driver fluentd host:%s.", host)
 	logrus.Debugf("logging driver fluentd tag:%s.", tag)
+
+	log, err := fluent.New(fluent.Config{FluentPort: port, FluentHost: host})
 	if err != nil {
 		return nil, err
 	}
